b6340d8d532d36a32099dda0fe76b2a34dfb06b6
Duresa Eshetu <duresaeshetu2001@gmail.com>
2024-03-10 00:06:56 +0300

    my first proffecional project

---
+++ portifolio/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import GlobalStyle from './globalStyle';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Nabvar from './components/navbar';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
